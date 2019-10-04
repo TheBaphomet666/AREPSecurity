6161d3a5803730b11e685cde26c28f67e5d55fdb
unknown <2125275@labinfo.is.escuelaing.edu.co>
2019-10-04 15:27:37 -0500

    Added SSL

---
+++ src/main/java/com/arep/AREPsecurity/ArePsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.arep.AREPsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArePsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArePsecurityApplication.class, args);
+	}
+
+}
